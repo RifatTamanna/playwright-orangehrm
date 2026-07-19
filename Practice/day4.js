aed06d6dba83cfeb49cdd6a84e09ca3773a75521
Rifat Tamanna <tamannarahman1928.com>
2026-07-19 17:53:05 +0600

    Added some elements practices - Javascript Day 4 Practice

--- Practice/day4.js
+++ Practice/day4.js
@@ -1,4 +1,4 @@
-// map(), filter(), find(), arrow function(=>)
+// arrow function(=>), map(), filter(), find(),
 
 
 // Arrow function
@@ -32,3 +32,37 @@
     let prices = [100, 200, 300, 400, 500];
     let taxedPrices = prices.map(price => Math.round(price * 1.1)); // chaining map() to apply tax and round to nearest integer
     console.log("Prices after tax:", taxedPrices);
+
+    let names = ["tamanna" , "taima" , "tarin" , "nourin"]
+    let upperNames = names.map(names => names.toUpperCase());
+    console.log("All upperNames", upperNames);
+
+
+//filter()
+
+    let ages = [15, 18, 20, 22, 12, 30, 40];
+    let adults = ages.filter( age => age >= 18);
+    console.log(adults);
+
+    let marks = [35, 50, 80, 25, 90, 60];
+    let pass = marks.filter(mark => mark >= 40);
+    console.log(pass);
+
+
+//find()
+
+    let browsers = ["Chrome", "Firefox", "Edge", "Safari"];
+    let browser = browsers.find(item => item === "Edge");
+    console.log("Browser: ", browser);
+    
+    let number = [10, 20, 30, 40, 50];
+    let result = number.find(num => num === 25);
+    console.log(result);
+
+    let employees = [
+    { id: 1, name: "Tamanna" },
+    { id: 2, name: "Rahim" },
+    { id: 3, name: "Karim" }
+    ];
+    let employee = employees.find(employee => employee.id ===2);
+    console.log(employee);
